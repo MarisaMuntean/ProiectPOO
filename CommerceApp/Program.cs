079e2f9c82262fa376bd40fc63457172007c4a0e
Marisa Muntean <munteanmarisa@gmail.com>
2024-12-28 12:59:28 +0200

    Created project alongside Utilizator, Admin and Client classes.

---
+++ CommerceApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CommerceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
