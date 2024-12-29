1b52cc9e17816b5efbea6535d7ea5259e1f7633e
Marisa Muntean <munteanmarisa@gmail.com>
2024-12-29 10:54:53 +0200

    Am creat clasa Utilizator si cele 2 ce o vor mosteni: Client si Admin. Am adaugat baza de date.

---
+++ ECommerceApp/Utilizatori/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Utilizatori
+{
+    internal class Admin : Utilizator
+    {
+        public Admin(string id, string nume, string prenume, string email, string telefon, string adresa, string tip) :
+            base(id, nume, prenume, email, telefon, adresa, tip)
+        { }
+    }
+}
---
+++ ECommerceApp/Utilizatori/Utilizator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Utilizatori
+{
+    internal abstract class Utilizator
+    {
+        public string ID { get; protected set; }
+        public string Nume { get; protected set; }
+        public string Prenume { get; protected set; }
+        public string Email { get; protected set; }
+        public string Telefon { get; protected set; }
+        public string Adresa { get; protected set; }
+        public string Tip { get; protected set; }
+
+
+        public Utilizator(string id, string nume, string prenume, string email, string telefon, string adresa, string tip)
+        {
+            this.ID = id;
+            this.Nume = nume;
+            this.Prenume = prenume;
+            this.Email = email;
+            this.Telefon = telefon;
+            this.Adresa = adresa;
+            this.Tip = tip;
+        }
+    }
+}
