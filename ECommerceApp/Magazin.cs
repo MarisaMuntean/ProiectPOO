7a4e6709592c2dd3b5d008dcadfec4805bcb23d1
MarisaMuntean <munteanmarisa@gmail.com>
2025-01-13 19:29:29 +0200

    Crearea claselor Magazin, Program
    +modificari la MagazinAdmin

---
+++ ECommerceApp/Magazin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp
+{
+    internal class Magazin
+    {
+
+        public void DeschidereAplicatie()
+        {
+            int identitate = IdentitateUtilizator();
+            if(identitate == 0)
+            {
+                MagazinClient magazinClient = new MagazinClient();
+                magazinClient.MagazinC();
+            }
+            else if(identitate == 1) 
+            {
+                MagazinAdmin magazinAdmin = new MagazinAdmin();
+                magazinAdmin.Magazin();
+            }
+            else
+            {
+                Console.WriteLine("Nu puteti accesa aplicatia!");
+            }
+        }
+        private int IdentitateUtilizator()
+        {
+            Console.WriteLine("Sunteti client? ");
+            string raspuns = Console.ReadLine().ToLower();
+            if (raspuns == "da")
+                return 0;
+            Console.WriteLine("Sunteti administrator?");
+            raspuns = Console.ReadLine();
+            if (raspuns == "da")
+                return 1;
+            return -1;
+        }
+    }
+}
---
+++ ECommerceApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceApp.MetodeAdmin;
+using ECommerceApp.Utilizatori;
+
+namespace ECommerceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Magazin aplicatie = new Magazin();
+            aplicatie.DeschidereAplicatie();
+        }
+    }
+}
